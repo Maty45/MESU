a0553d3c093077dd32692775e3b732bfdb9ffd93
MATI <MATI@DESKTOP-04Q22O8>
2026-06-02 17:15:11 -0300

    cambios developer

---
+++ backend/backend/src/main/java/com/models/TipoInsumo.java
@@ -0,0 +1,30 @@
+package com.models;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "tipoinsumo")
+public class TipoInsumo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer codTipoInsumo;
+    @Column(length = 200)
+    private String nombreTipoInsumo;
+    @Column
+    private LocalDateTime fechaHBajaTipoInsumo;
+
+    public TipoInsumo() {
+    }
+
+    public TipoInsumo(int codTipoInsumo, String nombreTipoInsumo, LocalDateTime fechaHBajaTipoInsumo) {
+        this.codTipoInsumo = codTipoInsumo;
+        this.nombreTipoInsumo = nombreTipoInsumo;
+        this.fechaHBajaTipoInsumo = fechaHBajaTipoInsumo;
+    }
+//    ignora este comentario
+}
